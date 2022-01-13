42a33a966df711fa210cd7c61713aca87c4f3cdd
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2022-01-12 22:42:13 -0600

    updated oh my posh theme

--- powershell/profile-ohMyPosh.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme C:\Users\knightj\source\repos\gh\jk\powershell\oh-my-posh-theme.omp.json
-
-. C:\Users\knightj\source\repos\gh\jk\powershell\Ionic-Functions.ps1
-Clear-Host
