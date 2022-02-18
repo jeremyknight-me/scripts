efc3fed3a2a98c89895d1c30c69e659b444187fd
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-02-18 09:36:51 -0600

    Create delete-from-large-table.sql

---
+++ sql/delete-from-large-table.sql
@@ -0,0 +1,13 @@
+/*
+    When possible use TRUNCATE. 
+    This method is designed for when TRUNCATE is not possible
+*/
+
+DECLARE @rowsDeleted INT = 1;
+WHILE (@rowsDeleted > 0)
+BEGIN
+    DELETE TOP(1000) -- delete X rows a time; here x = 1000
+	FROM [dbo].[YourTable]
+	where Id < 1000000; -- setup where claus as needed
+    SET @rowsDeleted = @@ROWCOUNT;
+END
