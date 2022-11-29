f2827e4ffd437190dafe8d0879d3ce079b2ba6e9
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-11-29 13:02:06 -0600

    created date dimension scripts using Date type instead of Int for Id (#5)

--- sql/dimensions/DateDimensionInt-Create.sql
+++ sql/dimensions/DateDimensionInt-Create.sql
@@ -6,7 +6,8 @@ GO
 
 CREATE TABLE [dbo].[DateDimension]
 ( 
-    [Id] DATE NOT NULL, 
+    [Id] INT NOT NULL, 
+    [DateStamp] DATE NOT NULL, 
     [DateName] CHAR(11) NOT NULL, 
     [DateNameUS] CHAR(11) NOT NULL,   --US Date Format: MM/DD/YYYY
     [DateNameEU] CHAR(11) NOT NULL,   --European Union Date Format: DD/MM/YYYY
@@ -30,6 +31,12 @@ CREATE TABLE [dbo].[DateDimension]
     [FiscalYearQtr] CHAR(6) NOT NULL, 
     CONSTRAINT [PK_DateDimension] PRIMARY KEY CLUSTERED ( [Id] )
 ) ON [PRIMARY]
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_DateDimension_DateStamp] ON [dbo].[DateDimension] 
+(
+	[DateStamp] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 GO
 
 /* NOTE: Add additional indexes per your use cases
