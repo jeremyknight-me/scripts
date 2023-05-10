1c005f3327895f9e80cbfc9a9a47c53b2940e5bf
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-05-10 12:54:45 -0500

    Delete get-net-framework-versions.ps1

--- powershell/get-net-framework-versions.ps1
+++
@@ -1,1 +0,0 @@
-Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where { $_.PSChildName -Match '^(?!S)\p{L}'} | Select PSChildName, version
