414ad63becef165a3c84b12d8ba44bdcd0d17b77
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2024-05-31 10:05:26 -0500

    Update profile-oh-my-posh.ps1

--- powershell/profile-oh-my-posh.ps1
+++ powershell/profile-oh-my-posh.ps1
@@ -1,3 +1,3 @@
-. ~\source\repos\gh\jk\scripts\powershell\Ionic-Functions.ps1
+# . ~\source\repos\gh\jk\scripts\powershell\Ionic-Functions.ps1
 oh-my-posh --init --shell pwsh --config "<PATH_TO_THEME_HERE>" | Invoke-Expression
 Clear-Host 
