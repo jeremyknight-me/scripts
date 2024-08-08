cd35f02580a36fe1feb0df5c000a0a37349cda51
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2024-08-08 15:09:30 -0500

    Update DateList.sql

--- sql/tables/DateList.sql
+++ sql/tables/DateList.sql
@@ -9,33 +9,41 @@ GO
 CREATE TABLE dbo.DateList  
 (  
     [Date] DATE NOT NULL,
-    [Year] SMALLINT,  
-    --YearFiscal SMALLINT,  
-    [Quarter] TINYINT,  
-	-- need fiscal quarter
-	[Month] TINYINT,  
-	[MonthText] VARCHAR(9),
-    [Day] TINYINT,  
-	[DayText] VARCHAR(9), 
-    [DateOfWeek] TINYINT,     
-    [Week] TINYINT,
-	[WeekIso] TINYINT,
-	IsWeekday BIT
+    [Year] SMALLINT NOT NULL,  
+    --YearFiscal SMALLINT NOT NULL,  
+    [Quarter] TINYINT NOT NULL,  
+    -- need fiscal quarter
+    [Month] TINYINT NOT NULL,  
+    [MonthText] VARCHAR(9) NOT NULL,
+    [Day] TINYINT NOT NULL,  
+    [DayText] VARCHAR(9) NOT NULL, 
+    [DateOfWeek] TINYINT NOT NULL,     
+    [Week] TINYINT NOT NULL,
+    [WeekIso] TINYINT NOT NULL,
+    IsWeekday BIT NOT NULL
 );
 
-/* --- insert number of record -------------------- */
+/* --- insert number of records -------------------- */
 
-INSERT dbo.DateList([Date]) 
-SELECT DATEADD(DAY, Number, '2009-12-31') 
-FROM dbo.NumberList
-WHERE Number <= 10957 -- ends on last day of 2039
-ORDER BY Number;
-
-/* --- add data for simple columns -------------------- */
-
-UPDATE dbo.DateList 
-SET 
-    [Year] = YEAR([Date]),
+INSERT dbo.DateList
+(
+    [Date], 
+    [Year], 
+    --YearFiscal SMALLINT,
+    [Quarter],  
+    -- need fiscal quarter
+    [Month],  
+    [MonthText],
+    [Day],  
+    [DayText], 
+    [DateOfWeek],     
+    [Week],
+    [WeekIso],
+    IsWeekday
+) 
+SELECT
+    a1.[Date],
+    [Year] = YEAR(a1.[Date]),
     --FiscalYear = YEAR(dt), 
     /* 
     -- if our fiscal year starts on May 1st: 
@@ -44,23 +52,29 @@ SET
         THEN YEAR(dt)-1  
         ELSE YEAR(dt) END, 
     */
-	[Quarter] = DATEPART(QUARTER, [Date]),
+    [Quarter] = DATEPART(QUARTER, a1.[Date]),
     --[FiscalQuarter] = CASE 
     --    WHEN MONTH([Date]) <= 3 THEN 1 
     --    WHEN MONTH([Date]) <= 6 THEN 2 
     --    WHEN MONTH([Date]) <= 9 THEN 3 
     --    ELSE 4 END,  
-    [Month] = MONTH([Date]),  
-    [MonthText] = DATENAME(MONTH, [Date]),
-	[Day] = DAY([Date]),  
-    [DayText] = DATENAME(DW, [Date]),  
-	[DateOfWeek] = DATEPART(DW, [Date]),  
-    [Week] = DATEPART(WEEK, [Date]),
-	[WeekIso] = DATEPART(ISO_WEEK, [Date]),
-	IsWeekday = CASE  
-        WHEN DATEPART(DW, [Date]) IN (1,7) THEN 0 
+    [Month] = MONTH(a1.[Date]),
+    [MonthText] = DATENAME(MONTH, a1.[Date]),
+    [Day] = DAY(a1.[Date]),  
+    [DayText] = DATENAME(DW, a1.[Date]),  
+    [DateOfWeek] = DATEPART(DW, a1.[Date]),  
+    [Week] = DATEPART(WEEK, a1.[Date]),
+    [WeekIso] = DATEPART(ISO_WEEK, a1.[Date]),
+    IsWeekday = CASE  
+        WHEN DATEPART(DW, a1.[Date]) IN (1,7) THEN 0 
         ELSE 1 
-    END;
+    END
+FROM dbo.NumberList
+    CROSS APPLY (VALUES(
+	    DATEADD(DAY, Number, '1989-12-31')
+    )) a1([Date])
+WHERE Number <= 21915 -- ends on last day of 2049
+ORDER BY Number;
 
 -- /* --- Week Start (monday of week) --------------- */
 -- DATEADD(wk, DATEDIFF(wk,0,[Date]), 0) MondayOfCurrentWeek
