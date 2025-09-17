999a0c09b878c2d2bb58a829a3b9fcd2105f513a
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2025-09-17 15:23:27 -0500

    added Update-GitRepos powershell function to profile

--- powershell/profile-oh-my-posh.ps1
+++ powershell/profile-oh-my-posh.ps1
@@ -1,3 +1,30 @@
+### Custom Functions --------------------------------------
+
+function Update-GitRepos {
+    param(
+        [string]$RootDirectory = (Get-Location)
+    )
+
+    $originalLocation = Get-Location
+
+    # Get all directories in the root directory
+    Get-ChildItem -Path $RootDirectory -Directory | ForEach-Object {
+        $folderPath = $_.FullName
+        $gitFolder = Join-Path $folderPath ".git"
+        if (Test-Path $gitFolder) {
+            Write-Host "Updating git repo in: $folderPath"
+            Set-Location $folderPath
+            git fetch
+            git pull
+        }
+    }
+
+    # Restore the original location
+    Set-Location $originalLocation
+}
+
+### Profile Settings and Configuration --------------------
+
 # . ~\source\repos\gh\jk\scripts\powershell\Ionic-Functions.ps1
 oh-my-posh --init --shell pwsh --config "<PATH_TO_THEME_HERE>" | Invoke-Expression
 
