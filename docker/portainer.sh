f54f2651826b111a989efdecd7e88104f8bafff1
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2026-02-25 11:17:33 -0600

    removed docker files

--- docker/portainer.sh
+++
@@ -1,5 +0,0 @@
-# latest lts
-docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-
-# latest sts
-docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:sts
